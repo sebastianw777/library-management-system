05f7177ac80c8a80dbb6cdc0f11b2609b099587d
sebastianw777 <walkiewiczseb0@gmail.com>
2026-07-17 22:56:23 +0200

    Add clients database page

---
+++ pages/clients.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+    header("Location: ../index.php");
+    exit();
+}  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Management System</title>
+</head>
+<body>
+
+<header>
+<h1>Library Management System</h1>
+    <nav>
+        <a href="books.php">Books database</a>
+        <a href="clients.php">Clients database</a>
+        <a href="../logout.php">Logout</a>
+        <?php
+            require_once "../config/config.php";
+            $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+            $login = $_SESSION['login'];
+            $query_name = "SELECT * FROM employees WHERE login = '$login';";
+            $result = mysqli_query($conn, $query_name);
+
+            $user = mysqli_fetch_assoc($result);
+            
+            echo "<p>Account: " .$user['first_name']. " " .$user['last_name']. "</p>";
+        ?>
+    </nav>
+</header>
+<main>
+<table>
+
+<tr>
+    <th>ID</th>
+    <th>First name</th>
+    <th>Last name</th>
+    <th>Email</th>
+    <th>Phone</th>
+    <th>Created at</th>
+</tr>
+<?php
+    $query = "SELECT * FROM clients;";
+
+    $result = mysqli_query($conn, $query);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><th>" .$row['id']. "</th><th>" .$row['first_name']. "</th><th>" .$row['last_name']. "</th><th>" .$row['email']. "</th><th>" .$row['phone']. "</th><th>" .$row['created_at']. "</th></tr>";
+    }
+
+    ?>
+
+    </table>
+</main>
+
+</body>
+</html>
